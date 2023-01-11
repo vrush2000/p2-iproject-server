da049c357b86f11d008cd4d6cdeea40eeb28ceb9
syamsul bahri <syamsul8895@gmail.com>
2023-01-11 17:15:55 +0700

    func get invitation by id done

--- controllers/controllerInvitation.js
+++ controllers/controllerInvitation.js
@@ -30,6 +30,42 @@ class Controller{
         }
     }
 
+    static async getInvitationById(req, res, next){
+        try {
+            let id = req.params.id  || ''
+            let data = await Invitations.findOne({
+                where: { id },
+                include: 
+                    [
+                        {model: Musics,
+                            attributes: {
+                                exclude: ['createdAt', 'updatedAt']
+                            }},
+                        {model: Users,
+                            include: {model:Greets,
+                            
+                        attributes: {
+                            exclude: ['createdAt', 'updatedAt']
+                        }},
+                        attributes: {
+                            exclude: ['password', 'role', 'createdAt', 'updatedAt']
+                        }}],
+                        attributes: {
+                            exclude: ['createdAt', 'updatedAt']
+                        }
+            })
+
+            if (!data) {
+                throw { name: "notFound" }
+            }
+
+            res.status(200).json(data)
+        } catch (error) {
+            console.log(error);
+            next(error)
+        }
+    }
+
 }
 
 module.exports = Controller
--- routes/invRouter.js
+++ routes/invRouter.js
@@ -4,6 +4,7 @@ const { authentication } = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', Controller.getInvitation);
+router.get('/:id', Controller.getInvitationById);
 // router.post('/:id', Controller.addInvitation);
 
 
