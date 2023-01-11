e712d7e805a03c3896064e36f6c44c858e2d13c6
syamsul bahri <syamsul8895@gmail.com>
2023-01-11 16:25:49 +0700

    func add greet done

--- controllers/controllerGreet.js
+++ controllers/controllerGreet.js
@@ -11,6 +11,20 @@ class Controller{
         }
     }
 
+    
+    static async addGreet(req, res, next){
+        try {
+            const UserId = req.params.id;
+            const { guest, presence, greeting } = req.body
+            let data = await Greets.create({ guest, presence, greeting, date: new Date(), UserId })
+            res.status(201).json(data)
+        } catch (error) {
+            console.log(error);
+            next(error)
+
+        }
+    }
+
 }
 
 module.exports = Controller
